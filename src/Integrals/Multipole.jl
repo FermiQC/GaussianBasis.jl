a0e3556f3b6d1d481cbed089c32509899f676a56
Lukas Weber <49278367+lukas-weber@users.noreply.github.com>
2024-05-28 11:25:39 -0400

    merge gen_multipole and get_1e_matrix

--- src/Integrals/Multipole.jl
+++ src/Integrals/Multipole.jl
@@ -1,40 +1,3 @@
-function gen_multipole(BS::BasisSet, callback, rank::Integer)
-    out = zeros(BS.nbas, BS.nbas, Iterators.repeated(3, rank)...)
-
-    Nvals = num_basis.(BS.basis)
-    Nmax = maximum(Nvals)
-
-    # Offset list for each shell, used to map shell index to AO index
-    ao_offset = [sum(Nvals[1:(i-1)]) for i = 1:BS.nshells]
-
-    buf_arrays = [zeros(Cdouble, 3^rank * Nmax^2) for _ = 1:Threads.nthreads()]
-
-    Threads.@threads :static for i in 1:BS.nshells
-        Ni = Nvals[i]
-        buf = buf_arrays[Threads.threadid()]
-        ioff = ao_offset[i]
-        for j in i:BS.nshells
-            Nj = Nvals[j]
-            Nij = Ni*Nj
-            joff = ao_offset[j]
-
-            callback(buf, BS, i, j)
-            I = (ioff+1):(ioff+Ni)
-            J = (joff+1):(joff+Nj)
-
-            # Get strides for each cartesian product
-            for (n, ks) in enumerate(Iterators.product(Iterators.repeated(1:3, rank)...))
-                r = Nij*(n-1)+1:n*Nij
-                kvals = reshape(view(buf, r), Ni, Nj)
-                out[I,J,ks...] .+= kvals
-                if i != j
-                    out[J,I,ks...] .+= kvals'
-                end
-            end
-        end
-    end
-    return out
-end
 
 dipole!(out, BS::BasisSet{LCint}, i, j) = cint1e_r_sph!(out, [i,j], BS.lib)
 quadrupole!(out, BS::BasisSet{LCint}, i, j) = cint1e_rr_sph!(out, [i,j], BS.lib)
@@ -62,7 +25,7 @@ function hexadecapole(BS::BasisSet, i, j)
     return out
 end
 
-dipole(BS::BasisSet) = gen_multipole(BS, dipole!, 1)
-quadrupole(BS::BasisSet) = gen_multipole(BS, quadrupole!, 2)
-octupole(BS::BasisSet) = gen_multipole(BS, octupole!, 3)
-hexadecapole(BS::BasisSet) = gen_multipole(BS, hexadecapole!, 4)
+dipole(BS::BasisSet) = get_1e_matrix(dipole!, BS, 1)
+quadrupole(BS::BasisSet) = get_1e_matrix(quadrupole!, BS, 2)
+octupole(BS::BasisSet) = get_1e_matrix(octupole!, BS, 3)
+hexadecapole(BS::BasisSet) = get_1e_matrix(hexadecapole!, BS, 4)
